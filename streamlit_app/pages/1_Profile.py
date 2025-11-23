ad650fb682e97cfaaf56c1b02c95a0c486656b85
saraapenha <saraapenha@gmail.com>
2025-11-23 17:33:02 +0100

    Update 1_Profile.py

--- streamlit_app/pages/1_Profile.py
+++ streamlit_app/pages/1_Profile.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.header("Your Profile")
 if "skin_type" not in st.session_state:
-    st.session_state["skin_type"]="oily"
-st.session_state["skin_type"]=st.selectbox("select skin type",["oily", "dry", "combination", "sensitive"], index=0)
-st.success("Selected Skin Type:"+ st.session_state["skin_type"])
+    st.session_state["skin_type"]="Oily"
+st.session_state["skin_type"]=st.selectbox("select skin type",["Oily", "Dry", "Combination", "Sensitive"], index=0)
+st.success("Selected Skin Type: "+ st.session_state["skin_type"])
